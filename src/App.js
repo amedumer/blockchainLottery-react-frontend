a56b4a346e93870055111f75409b5885941dba03
Ahmet Ömer Kayabaşı <69629296+amedumer@users.noreply.github.com>
2022-05-16 02:12:50 +0300

    contract connection with web3 is established contract manager address can be received

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Class implementation
+import "./App.css";
+import React from "react";
+import lottery from './lottery';
+
+class App extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {manager:""};
+    }
+
+    async componentDidMount() {
+        const manager = await lottery.methods.manager().call();
+        this.setState({manager});
+    }
+
+    render() {
+    return (
+            <div className="App">
+                <h2>Lottery Contract</h2>
+                <p>This contract is managed by {this.state.manager}</p>
+            </div>
+    );
+  }
+}
+export default App;
+
+
+/*
+// Functional implementation
+import "./App.css";
+import React, {useEffect, useState} from "react";
+import lottery from './lottery';
+
+function App(){
+
+    const [manager, setManager] = useState("");
+
+    useEffect(()=>{
+        const getManager = async () => {
+            const manager = await lottery.methods.manager().call();
+            setManager(manager);
+        }
+        getManager();
+    })
+
+
+    return(
+        <div className="App">
+            <h2>Lottery Contract</h2>
+            <p>This contract is managed by {manager}</p>
+        </div>
+    )
 }
 
 export default App;
+ */
---
+++ src/lottery.js
@@ -0,0 +1,105 @@
+import web3 from './web3';
+
+const address = "0xDEf4772f1ADF27c2e1f9f369b61207C730D633a6";
+
+const abi =[
+    {
+        "inputs": [],
+        "name": "enter",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "pickWinner",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [],
+        "name": "getPlayers",
+        "outputs": [
+            {
+                "internalType": "address payable[]",
+                "name": "",
+                "type": "address[]"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "id",
+                "type": "uint256"
+            }
+        ],
+        "name": "getWinner",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "lotteryIdx",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "manager",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "name": "players",
+        "outputs": [
+            {
+                "internalType": "address payable",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
+
+
+export default new web3.eth.Contract(abi,address);
